1dcd47162a514f425865c66393fe5acd287da5ba
Ilan-J <ilan.jaglin@viacesi.fr>
2022-10-21 12:32:54 +0200

    sql script to empty tables & modified drop order

---
+++ sql/delete_table_content.sql
@@ -0,0 +1,13 @@
+USE `test_grossiste_vin`;
+
+DELETE FROM `supply_list`;
+DELETE FROM `purchase_list`;
+
+DELETE FROM `supplier_command`;
+DELETE FROM `client_command`;
+
+DELETE FROM `supplier`;
+DELETE FROM `client`;
+
+DELETE FROM `product`;
+DELETE FROM `wine_family`;
