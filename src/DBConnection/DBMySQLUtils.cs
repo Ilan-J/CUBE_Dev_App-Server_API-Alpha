fe3e32ebf3dbc259c473a54bc28bf94644c576b6
Joachim-B <joachim.bernardlambert@gmail.com>
2022-10-21 15:59:38 +0200

    Changements organisation du code

---
+++ src/DBConnection/DBMySQLUtils.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Server_API.DBConnection
+{
+    class DBMySQLUtils
+    {
+
+        public static MySqlConnection GetDBConnection(string? host, int? port, string? database, string? username, string? password)
+        {
+            // Connection String.
+            string connString = "Server=" + host + ";Database=" + database
+                + ";port=" + port + ";User Id=" + username + ";password=" + password;
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            return conn;
+        }
+
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tutorial.SqlConn;
 using System.Data.Common;
 using System.Data;
 using MySql.Data.MySqlClient;
+using Server_API.DBConnection;
 
 namespace CsMySQLTutorial
 {
@@ -25,37 +25,7 @@ namespace CsMySQLTutorial
 
             try
             {
-                /*
-                // La commande Insert.
-                string sql = "INSERT INTO client (name, surname, address, postal_code, town, email, password)"
-                    + "values (@name, @surname, @address, @postal_code, @town, @email, @password)";
-
-
-                MySqlCommand cmd = conn.CreateCommand();
-                cmd.CommandText = sql;
-
-                // Créez un objet Paramètre.
-                MySqlParameter gradeParam = new MySqlParameter("@name", SqlDbType.VarChar);
-                gradeParam.Value = "Dujardin";
-                cmd.Parameters.Add(gradeParam);
-
-                // Ajoutez le paramètre @highSalary (Écrire plus court).
-                MySqlParameter highSalaryParam = cmd.Parameters.Add("@highSalary", (MySqlDbType)SqlDbType.Float);
-                highSalaryParam.Value = 20000;
-
-                // Ajoutez le paramètre @lowSalary (Écrire plus court).
-                cmd.Parameters.Add("@lowSalary", (MySqlDbType)SqlDbType.Float).Value = 10000;
-                */
-
-                // Exécutez la Commande (Utilisez pour supprimer, insérer, mettre à jour).
-                string sql = "INSERT INTO client (name, surname, address, postal_code, town, email, password) values ('Dujardin', 'Jean', '1 Rue de la Liberté','75001', 'Paris', 'jd@gmail.com', 'coucou')";
-                
-                MySqlCommand cmd = conn.CreateCommand();
-                cmd.CommandText = sql;
-
-                int rowCount = cmd.ExecuteNonQuery();
-
-                Console.WriteLine("Row Count affected = " + rowCount);
+                Console.WriteLine("Connexion à la base de données réussie.");
             }
             catch (Exception e)
             {
